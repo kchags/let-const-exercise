0378a7e0d55e1121d17301e14b8dbf5292d5ec67
Kevin Chagas <kevinchagas@yahoo.comKevinKevin>
2022-11-27 09:57:49 -0600

    let const

---
+++ let-const-exercise.js
@@ -0,0 +1,13 @@
+const PI = 3.14
+
+//What is the difference between var and let?
+//you cannot redeclare when using Let
+
+//What is the difference between car and const?
+//you cannot redeclare or reassign when using const
+
+//What is the difference between let and const?
+//you can reassign let but not const
+
+//What is hoisting?
+//it is how the JS compiler works, variables are lifting to the top of the scope they are declared in.
